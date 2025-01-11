8596cb647bcdfccc583315980943ad4a8099d768
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-11 13:34:14 +0530

    mounting and unmounting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
+import { useState } from 'react';
 
 function App() {
+  const [state,setState] = useState(false)
   return (
     <div>
-      Hi
+      <h2 onClick={()=>setState(!state)}>Show / Hide</h2>
+      { state && <Counter /> }
     </div>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Counter() {
+  return (
+    <div>
+      <h1>Hellow iam a component !</h1>
+    </div>
+  )
+}
+
+export default Counter
