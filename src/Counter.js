f2e69ba8f216f0c33de14069f4287a189a503f92
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-13 10:16:27 +0530

    useEffect is used while mounting,updating,unmounting - 3

--- src/Counter.js
+++ src/Counter.js
@@ -23,19 +23,23 @@ import React, { useState, useEffect } from 'react';
 
 function Counter() {
     const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
 
     useEffect(() => {
         console.log("It's mounting ....");
-        console.log("Updated : " + count)
-        return () => {
-            console.log("Clean up - " + count);
-        };
-    }, [count]);
+        console.log("1 -count : " + count)
+        console.log("2 -count : " + count2)
+        // return () => {
+        //     console.log("Clean up - " + count);
+        // };
+    }, [count,count2]);
 
     return (
         <div>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <h1>Hello, I am a counter: {count}</h1>
+            <button onClick={() => setCount2(count2 + 1)}>Increment</button>
+            <h1>Hello, I am a counter two: {count2}</h1>
         </div>
     );
 }
