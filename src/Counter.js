e861d9789d537bdf4ef0812e6693c3a1c02f34b8
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-13 10:02:12 +0530

    useEffect is used while mounting,updating,unmounting

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,42 @@
-import React,{useState} from 'react'
+// import React,{useState,useEffect} from 'react'
+
+// function Counter() {
+//     useEffect(()=>{
+//         console.log("Its mounting ....");
+//         return()=>{
+//             console.log("Its unmounting ....");
+//         }
+//     },[])
+//     const [count,setCount] = useState(0)
+//     return (
+//         <div>
+//             <button onClick={()=>setCount(count+1)}>Incriment</button>
+//             <h1>Hellow iam a counter : {count}</h1>
+//         </div>
+//     )
+// }
+
+// export default Counter
+
+
+import React, { useState, useEffect } from 'react';
 
 function Counter() {
-    const [count,setCount] = useState(0)
-  return (
-    <div>
-        <button onClick={()=>setCount(count+1)}>Incriment</button>
-        <h1>Hellow iam a counter : {count}</h1>
-    </div>
-  )
+    useEffect(() => {
+        console.log("It's mounting ....");
+        return () => {
+            console.log("It's unmounting ....");
+        };
+    }, []);
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <h1>Hello, I am a counter: {count}</h1>
+        </div>
+    );
 }
 
-export default Counter
+export default Counter;
